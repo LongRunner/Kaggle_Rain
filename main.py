59a6f40eb01fd73a0f3b0b43b4add8a0345badf8
longrunner <isaiah@cs.utexas.edu>
2015-02-11 21:59:57 -0600

    reorganized codebase

--- main.py
+++ main.py
@@ -1,50 +1,17 @@
-import numpy as np
-import pickle
+import tools
 import time
-
-def parse_file(file_name):
-    with open(file_name, 'r') as f:
-        read_data = f.read()
-    f.closed
-    out = []
-    labels = []
-    file_lines = read_data.split('\n')
-    for row in range(len(file_lines)):
-        if row >= 1:
-            out_temp = [[]]
-            col_data = file_lines[row].split(',')
-            if len(col_data)>=2:
-                repeat = len(col_data[1].split(' '))
-            else:
-                print(col_data)
-                print(len(col_data))
-                print('\n')
-                continue
-            out_temp = [x[:] for x in [[None] * 20] * (repeat)]
-            id_n = float(col_data[0])
-            exp_rain = float(col_data[len(col_data) - 1])
-            for col in range(len(col_data)):
-                for i in range(repeat):
-                    if col == 0:
-                        out_temp[i][col] = id_n
-                    elif col == (len(col_data) - 1):
-                        out_temp[i][col] = exp_rain
-                    else:
-                        out_temp[i][col] = col_data[col].split(' ')[i]
-            out += out_temp
-        else:
-            labels = file_lines[row].split(',')
-    return {'data': np.array(out), 'labels': labels}
-
-
-def save_file(name, object):
-    np.save(name + '.npz', object['data'])
-    pickle.dump(object['labels'], open(name + ".p", "wb"))
-
-
-def load_file(name):
-    return {'data': np.load(name+'.npz') ,'labels':pickle.load(open(name + ".p", "rb"))}
+import numpy as np
 
 if __name__ == '__main__':
-    data_dict = parse_file('train_2013.csv')
-    save_file('train_2013', data_dict)
+    '''
+    #Parse Data and Save Data
+    data_dict = tools.parse_file('train_2013.csv')
+    tools.save_file('train_2013', data_dict)
+    np.savetxt("processed_train_2013.csv", data_dict['data'], delimiter=",")
+    '''
+    data_dict = tools.load_file('train_2013')
+    data_dict['pca_data'] = tools.pca_norm(np.nan_to_num(data_dict['data'][1:10000,1:19]), k = 10)
+    data_dict['pca_data'].norm()
+    print(data_dict['pca_data'].exp_var)
+    gen = (i for i,k in zip(range(len(data_dict['pca_data'].exp_var)),data_dict['pca_data'].exp_var) if k*100>90.0)
+    print(next(gen)+1)
